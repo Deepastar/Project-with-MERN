b014a0134d5aa4478bf0a2633187f394931df9d7
Deepa Ravichandran <deepa5starbee@gmail.com>
2018-09-26 14:24:26 -0400

    added contact us section

--- client/src/components/Pages/ContactUs.js
+++ client/src/components/Pages/ContactUs.js
@@ -1,14 +1,37 @@
 import React from "react";
 
 const ContactUs = () => (
-    <div className="text-center mt-5">
-        <h1>Contact Us</h1>
-        <div>
-            <strong>Our Mission</strong>
-            <p>
-                To save our customers as much time as possible by providing the most convenient car wash services available.
-            </p>
-        </div>
-    </div>
+<div className="form mt-5">
+<h1>Contact Us</h1>
+
+{/* <p>
+Leave us a message:
+</p> */}
+<form>
+<div class="form-group">
+<label for="nameInput">Full Name</label>
+<input type="name" class="form-control" id="nameForm" placeholder="Enter your Name"></input>
+</div>
+
+<div class="form-group">
+<label for="emailInput">Email Address</label>
+<input type="email" class="form-control" id="emailForm" placeholder="Enter your Email ID"></input>
+<small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+</div>
+
+<div className="form-group">
+<label for="messageInput">Message:</label>
+{/* <input type="message" class="form-control" id="emailForm" placeholder="Enter your Email ID"></input> */}
+<div className="comment mt-3">
+<textarea rows="4" cols="50" name="comment" form="usrform">
+Enter text here...</textarea>
+</div>
+</div>
+
+
+</form>
+
+
+</div>
 );
 export default ContactUs;
--- client/src/components/Pages/ServicePage.js
+++ client/src/components/Pages/ServicePage.js
@@ -1,14 +1,58 @@
 import React from "react";
+import "./CSS/service.css";
 
 const ServicePage = () => (
     <div className="text-center mt-5">
-        <h1>Services</h1>
-        <div>
-            <strong>Service 1</strong>
-            <p>
-                Basic Car Wash
-            </p>
+       
+        <div className="card" id="service">
+        <div className="card-body">
+        <h3 className="card-title text-left">Quick/Basic  Wash</h3>
+        <p>We love Quick Quack Car Wash! The wash is fast and efficient and does a great job with all of our customer's car.
+        We have been there over orlando and have never been disappointed. We wash everything outside of your car including wheels. 
+        This service makes your car to be very clean outside ie. "Looks Good Feels Good"
+        </p>
+        <h3 className="card-title text-left">Premium Car Wash</h3>
+        <p>Here you can get additionally with hot wax and shine. Most of the people will go for this service according to our survey in orlando.
+         Once  we finish our wash here, you will be wondering at your car with extraordinary shining.
+         Its lilke a "WOW" service. And affordable too!!!!
+        </p>
+        
+        <h3 className="card-title text-left"> Ultra Premium Car Wash</h3>
+        <p>Here the service takes couple of hours to make it a fresh one. You need to have some patience 
+         that we need to finish and handle to you. This is extremely very shining once we done the wash.
+         It includes vaccum with wax.
+        </p>
+        <h3 className="card-title text-left"> Factory Detailing</h3>
+        <p>
+           This service gives you the car back as like brand new car. It includes everything regarding wash with vaccum and wax. It completely protects from raining.
+           Its extremely a hot service that we do. We take additional care to your car to make it a very fresh and new one.
+           It will take 3 to 4 hours to finish this wash. But we promise you that it will an unbelivable service. you will love it!!!!!
+        </p>
         </div>
+        </div>
+
+
+        {/* <link type="link" data-toggle="modal" data-target="#exampleModalCenter">{ServicePage}
+        </link>
+
+        <div className="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+        <div className="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+        <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalCenterTitle">ServicePage</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+        </button>
+        </div>
+        <div class="modal-body">
+        ....
+        </div>
+        <div className="modal-footer">
+        <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+        </div>
+        </div>
+        </div>
+        </div> */}
     </div>
 );
 export default ServicePage;
